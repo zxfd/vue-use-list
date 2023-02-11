1e81f7816498e471dc1f195532f074f78bf26c48
yuexiangrong <russellyue@qq.com>
2023-02-11 16:06:31 +0800

    feat(core): ✨ 增加parseList

--- core/index.ts
+++ core/index.ts
@@ -1,3 +1,24 @@
 export default () => {
-  console.log('this is the core')
-}
+  console.log('this is the core');
+};
+
+export const parseList = <TRes extends object, TItem extends object>(
+  res: TRes,
+  attrChain: string
+): TItem[] => {
+  const errorMessage = 'invalid attr key';
+  if (attrChain.length === 0) {
+    throw new Error(errorMessage);
+  }
+  let obj: any = res;
+  attrChain.split('.').forEach((el) => {
+    if (!obj[el]) {
+      throw new Error(errorMessage);
+    }
+    obj = obj[el];
+  });
+  if (!(obj instanceof Array)) {
+    throw new Error(errorMessage);
+  }
+  return obj as TItem[];
+};
