836a295764cad3fca53f37a3e543f7998f354f7e
八宝粥 <zxiaofoo@qq.com>
2023-02-11 16:27:35 +0800

    feat(commonConfig): ✨ 添加通用的默认配置

---
+++ core/commonDefault.ts
@@ -0,0 +1,8 @@
+import type { CommonConfig } from "@vue-use-list/types";
+
+export const commonConfig: CommonConfig = {
+  delay: 0,
+  init: true,
+  pageSize: 10,
+  params: {},
+}
--- types/index.ts
+++ types/index.ts
@@ -6,5 +6,5 @@ export interface CommonConfig {
   pageSize?: number; // 默认10
   init?: boolean; // 是否需要初始化,默认true
   delay?: number; // 延迟多久初始化
-  params?: Ref | ComputedRef; // 筛选参数，需要支持双向绑定，在发起请求的时候会携带上
+  params?: Ref | ComputedRef | Record<string, any>; // 筛选参数，需要支持双向绑定，在发起请求的时候会携带上
 }
