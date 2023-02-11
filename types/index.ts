808ca1e32ece98b20cc7f3e69bda75468ad857d2
yuexiangrong <russellyue@qq.com>
2023-02-11 16:16:04 +0800

    refactor: 🦄 data->params

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,10 @@
-import type { Ref, ComputedRef } from 'vue'
+import type { Ref, ComputedRef } from 'vue';
 
-export type ReqFun = (...args: any[]) => Promise<any>
+export type ReqFun = (...args: any[]) => Promise<any>;
 
 export interface CommonConfig {
-  pageSize?: number // 默认10
-  init?: boolean // 是否需要初始化,默认true
-  delay?: number // 延迟多久初始化
-  data?: Ref | ComputedRef // 筛选参数，需要支持双向绑定，在发起请求的时候会携带上
+  pageSize?: number; // 默认10
+  init?: boolean; // 是否需要初始化,默认true
+  delay?: number; // 延迟多久初始化
+  params?: Ref | ComputedRef; // 筛选参数，需要支持双向绑定，在发起请求的时候会携带上
 }
